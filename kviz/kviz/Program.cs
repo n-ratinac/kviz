a9eb914f03f24e2d7b49943ef3308e7b6dcdb115
n-ratinac <nikola.ratinac@prvagimnazija.edu.rs>
2024-12-05 18:14:52 +0100

    kreiran projekat

--- kviz/kviz/Program.cs
+++ kviz/kviz/Program.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace quiz
+namespace kviz
 {
-    internal class Pitanje
+    class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }

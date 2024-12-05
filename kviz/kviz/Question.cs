c9ec481567b1b85d519ef67dde919c187596835a
Ucenik-IK5-09 <tamara.m.cica@gmail.com>
2024-12-05 18:54:21 +0100

    Question Attribute

--- kviz/kviz/Question.cs
+++ kviz/kviz/Question.cs
@@ -7,5 +7,11 @@ namespace kviz
 {
     class Question
     {
+        public string Text { get; private set; }
+        public List<string> Answers { get; private set; }
+        public int CorrectIndex { get; private set; }
+        public int PointValue { get; private set; }
+
+
     }
 }

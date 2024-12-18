38ab5dc8bcb38b8d911eae1316bcf502d30becd2
lenastankovic2007 <lenastankovic2007@gmail.com>
2024-12-18 12:02:34 +0100

    zavrsena klasa questionbank

--- kviz/kviz/QuestionBank.cs
+++ kviz/kviz/QuestionBank.cs
@@ -36,7 +36,29 @@ namespace kviz
 
         public List<Question> RandomQuestions(int n, int m)
         {
+            List<Question> NList = new List<Question>();
 
+            for (int i = 0; i < QList.Count; i++)
+            {
+                if (QList[i].PointValue == m * 10)
+                {
+                    NList.Add(QList[i]);
+                }
+            }
+
+            if (n > NList.Count)
+            {
+                Console.WriteLine("Trazen veci broj pitanja nego sto postoji");
+                return NList;
+            }
+            
+            Random rnd = new Random();
+            for (int i = 0; i < NList.Count - n; i++)
+            {
+                NList.RemoveAt(rnd.Next(0, NList.Count));
+            }
+
+            return NList;
 
         }
     }
